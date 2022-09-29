310d6f1508cffb23922d38dfd76c19994063ee16
Hans Tanawi <hans.tanawi@outlook.com>
2022-09-29 15:48:01 +0800

    feat: add user services to user routes

--- src/api/users/users.routes.ts
+++ src/api/users/users.routes.ts
@@ -1,25 +1,52 @@
-import type { Request, Response } from 'express';
+import { User } from '@prisma/client';
+import type { NextFunction, Request, Response } from 'express';
 import { Router } from 'express';
+
+import * as UsersService from './users.service';
 
 const router = Router();
 
 // GET /users
-router.get('/', (req: Request, res: Response) => {
-  return res.status(200).json({ message: 'Get users endpoint' });
+router.get('/', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const users = await UsersService.findAllUsers();
+    return res.status(200).json(users);
+  } catch (err: any) {
+    next(err);
+  }
 });
 
 // GET /users/:id
-router.get('/', (req: Request, res: Response) => {
-  return res.status(200).json({ message: 'Get user endpoint' });
+router.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const userId = req.params.id as string;
+    const user = await UsersService.findUserById(userId);
+    return res.status(200).json(user);
+  } catch (err: any) {
+    next(err);
+  }
 });
 
 // POST /users
-router.post('/', (req: Request, res: Response) => {
-  return res.status(200).json({ message: 'Create user endpoint' });
+router.post('/', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const newUser = req.body as Omit<User, 'id'>;
+    const user = await UsersService.createUser(newUser);
+    return res.status(201).json(user);
+  } catch (err: any) {
+    next(err);
+  }
 });
 
-router.put('/:id', (req: Request, res: Response) => {
-  return res.status(200).json({ message: 'Update user endpoint' });
+router.put('/:id', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const userId = req.params.id as string;
+    const updatedUser = req.body as Omit<User, 'id'>;
+    const user = await UsersService.updateUser(userId, updatedUser);
+    return res.status(201).json(user);
+  } catch (err: any) {
+    next(err);
+  }
 });
 
 export default router;
