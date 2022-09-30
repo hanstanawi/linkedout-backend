39a2230ef4869d1c9872911e3ce3e8a677e9e739
Hans Tanawi <hans.tanawi@outlook.com>
2022-09-30 18:46:39 +0800

    fix: experience model validator rule

--- src/api/experiences/experiences.model.ts
+++ src/api/experiences/experiences.model.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 // Only for validation purpose
 export const ExperienceModel = z.object({
   jobTitle: z.string().min(1),
-  jobDescription: z.string().min(1),
+  jobDescription: z.string().min(1).nullable(),
   startDate: z.string(),
   endDate: z.string().nullable(),
   isCurrent: z.boolean(),
